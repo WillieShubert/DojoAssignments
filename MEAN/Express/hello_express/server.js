d81665536a1882f638d93887f53496ae5201262d
willieshubert <willie.pictograms@gmail.com>
2017-03-10 11:31:46 -0800

    MEAN Express

--- MEAN/Express/hello_express/server.js
+++ MEAN/Express/hello_express/server.js
@@ -2,10 +2,27 @@ var express = require("express");
 
 var app = express();
 
+app.use(express.static(__dirname + "/static"));
+
+// This sets the location where express will look for the ejs views
+app.set('views', __dirname + '/views');
+// Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
+app.set('view engine', 'ejs');
+
+app.get("/users", function (request, response){
+    // hard-coded user data
+    var users_array = [
+        {name: "Michael", email: "michael@codingdojo.com"},
+        {name: "Jay", email: "jay@codingdojo.com"},
+        {name: "Brendan", email: "brendan@codingdojo.com"},
+        {name: "Andrew", email: "andrew@codingdojo.com"}
+    ];
+    response.render('users', {users: users_array});
+})
+
 app.get('/', function(request, response){
 	response.send("Hello Express")
 })
-
 app.listen(8000, function(){
-	console.log(express);
+	console.log("listening at port 8000");
 })
