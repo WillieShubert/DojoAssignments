9b528af28c1cfb2c9870cd2b894fa08985e5028a
willieshubert <willie.pictograms@gmail.com>
2017-04-17 10:20:27 -0700

    rails tdd belt week

---
+++ Ruby/Rails_TDD/TestingFactoryApp/app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = ['User successfully created']
+      redirect_to user_path(User.ids)
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to :back
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
+end
---
+++ Ruby/Rails_TDD/TestingFactoryApp/spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    last_name "Doe"
+    first_name "John"
+    email "john@doe.com"
+    end
+end
